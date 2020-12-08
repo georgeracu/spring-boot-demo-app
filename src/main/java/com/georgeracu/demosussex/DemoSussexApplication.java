077c9ef8cdb0cdfe8f9a2eb2f300b2842a775e9e
Georgica Racu <racu.george@gmail.com>
2020-12-09 00:18:15 +0200

    Initial commit

---
+++ src/main/java/com/georgeracu/demosussex/DemoSussexApplication.java
@@ -0,0 +1,13 @@
+package com.georgeracu.demosussex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSussexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSussexApplication.class, args);
+	}
+
+}

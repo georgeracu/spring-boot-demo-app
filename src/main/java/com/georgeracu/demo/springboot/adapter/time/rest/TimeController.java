8d1d0cd60d44f40f39a1412d5383aaf6f6845612
George <racu.george@gmail.com>
2022-03-24 20:17:49 +0100

    TDD: Exercise 1
    - Add a new GET endpoint that will return a JSON response with the
    timestamp at the time of call. The return field should be called
    `timestamp`

---
+++ src/main/java/com/georgeracu/demo/springboot/adapter/time/rest/TimeController.java
@@ -0,0 +1,12 @@
+package com.georgeracu.demo.springboot.adapter.time.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Author georgeracu
+ */
+@Controller
+@RequestMapping("/api/v1/time")
+public class TimeController {
+}
